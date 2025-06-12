1d4dd8f85da505b5e692e8d4c894900098e5cc3e
JR Mayberry <rimayber@microsoft.com>
2025-06-12 12:43:07 +0900

    fix wrong sensor in HA

--- tests/local_descriptions_object.py
+++ tests/local_descriptions_object.py
@@ -84,271 +84,271 @@ LOCAL_DESCRIPTIONS = [
         "local_description": "Local Broadcast Address"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.49.1",
         "category": "FIREWALL",
         "local_description": "PfSense Firewall"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
-        "ip_address": "192.168.49.80",
+        "api_endpoint": "http://192.168.50.220:8044",
+        "ip_address": "192.168.50.220",
         "category": "SERVER",
         "local_description": "Docker 80 Server Ubuntu/Docker"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.4",
         "category": "DOORBELL_CAMERA",
         "local_description": "Nest Doorbell Frontyard"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.6",
         "category": "TABLET",
         "local_description": "Hiace Galaxy Tablet Samsung"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.7",
         "category": "DOORBELL_CAMERA",
         "local_description": "Next Doorbell Frontdoor"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.9",
         "category": "LIGHT_CONTROLLER",
         "local_description": "Philips Hue Light Controller"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.13",
         "category": "LIGHT_CONTROLLER",
         "local_description": "Panasanic Advance Light Controller"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.51.255",
         "category": "BROADCAST_ADDRESS",
         "local_description": "Broadcast Address for 192.168.48.0/22"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.220",
         "category": "SERVER",
         "local_description": "Frigate Unbuntu Server/Docker"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.217",
         "category": "ZIGBEE_RADIO",
         "local_description": "SLZB-06P7 POE Adapter"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.216",
         "category": "LIGHT_CONTROLLER",
         "local_description": "Sunvie Outdoor Lighting Controller with Tuya Zigbee Relay"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.173",
         "category": "DESKTOP_COMPUTER",
         "local_description": "Windows HP Desktop Computer"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.149",
         "category": "LAPTOP",
         "local_description": "SAW Laptop"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.129",
         "category": "TV",
         "local_description": "TCL 85 Inch TV"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.124",
         "category": "HOME_ASSISTANT",
         "local_description": "Outdoor Alexa Echo Dot"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.113",
         "category": "AIR_PURIFIER",
         "local_description": "Coway Air Purifier"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.110",
         "category": "WIFI_ACCESS_POINT",
         "local_description": "Unifi U6 Pro AP"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.111",
         "category": "WIFI_ACCESS_POINT",
         "local_description": "Unifi U6 Pro AP"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.107",
         "category": "MOBILE_PHONE",
         "local_description": "Thao's iPhone"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.101",
         "category": "REFRIGERATOR",
         "local_description": "Sharp Refrigerator"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.87",
         "category": "HOME_ASSISTANT",
         "local_description": "Amazon Bedroom Echo Dot"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.82",
         "category": "MOBILE_PHONE",
         "local_description": "Huawei Pura Ultra 70"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.81",
         "category": "BLUETOOTH_RADIO",
         "local_description": "ESP32 Bluetooth Proxy"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.79",
         "category": "PRESENCE_SENSOR",
         "local_description": "Aqara FP2"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.78",
         "category": "PRESENCE_SENSOR",
         "local_description": "Aqara FP2"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.77",
         "category": "PRESENCE_SENSOR",
         "local_description": "Aqara FP2"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.76",
         "category": "PRESENCE_SENSOR",
         "local_description": "Aqara FP2"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.61",
         "category": "HOME_ASSISTANT",
         "local_description": "Google Nest Hub"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.54",
         "category": "WATER_HEATER",
         "local_description": "Rinnai Water Heater"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.47",
         "category": "PLUG",
         "local_description": "Tuya Smart Plug"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.49",
         "category": "ENERGY_MONITOR",
         "local_description": "Emporia Vue Gen 2"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.43",
         "category": "PLUG",
         "local_description": "Tuya Smart Plug"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.42",
         "category": "PLUG",
         "local_description": "Tuya Smart Plug"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.41",
         "category": "PLUG",
         "local_description": "Tuya Smart Plug"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.40",
         "category": "PLUG",
         "local_description": "Tuya Smart Plug"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.38",
         "category": "SERVER",
         "local_description": "LLM Ubuntu Server/Docker"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.37",
         "category": "WEATHER_STATION",
         "local_description": "Ambient Weather Station"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.36",
         "category": "SPEAKER",
         "local_description": "JBL 1000"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.35",
         "category": "SPRINKLER_CONTROLLER",
         "local_description": "Rachio Sprinkler Controller"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.15",
         "category": "AIR_CONDITIONER",
         "local_description": "Panasonic Air Conditioner"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.17",
         "category": "LAPTOP",
         "local_description": "Thinkpad X1 Laptop"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.18",
         "category": "AIR_CONDITIONER",
         "local_description": "Panasonic Air Condition"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.25",
         "category": "MOBILE_PHONE",
         "local_description": "Samsung ZFold"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.27",
         "category": "PET_FEEDER",
         "local_description": "Petkit Duo"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.49.100",
         "category": "SWITCH",
         "local_description": "QNAP QSW-M2116P-2T2S"
@@ -360,13 +360,13 @@ LOCAL_DESCRIPTIONS = [
         "local_description": "Huawei Pura Ultra 70"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.65",
         "category": "SCALE",
         "local_description": "Garmin Scale"
     },
     {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.31",
         "category": "PRINTER",
         "local_description": "Canon Printer"
@@ -378,7 +378,7 @@ LOCAL_DESCRIPTIONS = [
         "local_description": "Azure Cloud Server Docker"
     },
      {
-        "api_endpoint": "http://192.168.49.80:8044",
+        "api_endpoint": "http://192.168.50.220:8044",
         "ip_address": "192.168.50.3",
         "category": "DRONE",
         "local_description": "DJI Mavic 3"
